16790770fc79202595fb4444843b59f5cddb8ab6
David <dholme25@gmail.com>
2021-07-20 08:24:11 -0400

    Modified rides.html and script.js to begin adding buttons to hide and show ride thumbnails.

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
-let moreVideos = document.querySelectorAll(".more-videos");
+
 let showMoreButton = document.querySelector(".show-more");
 let showFewerButton = document.querySelector(".show-fewer");
 
-showMoreButton.addEventListener('click', function () {
-  moreVideos.style.display = "flex";
-})
+/* for (let i = 0; i < ) {
+  showMoreButton.addEventListener('click', function () {
+    moreVideos.style.display = "flex";
+  })
+}*/
+
+function showVideos() {
+  let moreVideos = document.getElementsByClassName("more-videos");
+  if (moreVideos.style.display === "none") {
+    moreVideos.style.display = "flex";
+  } else {
+    moreVideos.style.display = "none";
+  }
+}
